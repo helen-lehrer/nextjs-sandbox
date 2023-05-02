d3e695f0f411d0302064101e50d73c12336403d0
Helen Lehrer <hlehrer@outlook.com>
2023-05-02 15:20:25 -0700

    Add global styles

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyonload"
+        strategy="lazyOnload"
         onLoad={() => 
           console.log(`script loaded correctly, window.FB has been populated`)
         }  
@@ -19,6 +20,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
